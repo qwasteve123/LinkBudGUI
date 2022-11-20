09f80c59720917bc18ccf5a17a363648ed8069d8
qwasteve <waitoau01@gmail.com>
2022-11-21 01:58:31 +0800

    create canvas module

--- program/GUI.py
+++ program/GUI.py
@@ -8,9 +8,7 @@ import os
 import time
 from PIL import ImageTk,Image
 from relative_grid import *
-
-
-
+from canvas import *
 
 def convert(file, outputDir):
     outputDir = outputDir + str(round(time.time())) + '/'
@@ -31,117 +29,6 @@ class HexColor(Enum):
     TITLE = '#222933'
     TOOLTAB = '#3B4453'
     CANVAS_BACKGROUD = '#222831'
-class WindowCanvas():
-    def __init__(self,app,width,height,row,column,sticky):
-        self.width = width
-        self.height = height
-        self.bkgd_color = HexColor.CANVAS_BACKGROUD.value
-        self.MAX_ZOOM = 30
-        self.MIN_ZOOM = -25
-        self.scale_step = 0
-        self.ZOOM_SCALE = 1.1
-        self.initialdir= './imag/'
-        self.is_hover = False
-        self.row = row
-        self.column = column
-        self.app = app
-        self.left_click_mode = 'pan'
-        self.draw_x1, self.draw_y1 = None,None
-
-        self.canvas = Canvas(app, 
-                        width=self.width,height=self.height, 
-                        background=self.bkgd_color,
-                        cursor='tcross',borderwidth=0,highlightthickness=0,
-                        relief=FLAT)
-        self.canvas.grid(row=row,column=column,columnspan=2,sticky=sticky)  
-        self.canvas.bind("<MouseWheel>", self.mouse_wheel)
-        self.canvas.bind("<B1-Motion>", self.pan_move)
-        self.canvas.bind("<B1-ButtonRelease>", self.pan_release)
-        self.canvas.bind("<B2-Motion>", self.pan_move)
-        self.canvas.bind("<B2-ButtonRelease>", self.pan_release)
-        self.canvas.bind("<Button-3>", self.drawline)
-        self.canvas.bind("a",self.draw_rectangle)
-        self.canvas.bind("<Control-A>",self.showkey)
-        self.canvas.bind("<Motion>", self.hover_motion)
-        self.canvas.bind("<Leave>", self.hover_leave)
-        self.label_coor = ttk.Label(app,text='Coordinates  x: ---  y: ---')
-        self.label_coor.grid(row=row,column=column+1,sticky=SE)
-        self.label_status = ttk.Label(app,text='Status: pan')
-        self.label_status.grid(row=row,column=column,sticky=SW)
-        self.world_grid = WorldGrid(self.width,self.height,self.canvas)
-    def showkey(self,event):
-        print(event.keysym)
-
-    def hover_motion(self,event):
-        x,y = self.world_grid.screen_to_world(event.x,event.y,self.scale_step,self.width,self.height)
-        x += self.world_grid.screen_center_world_x
-        y += self.world_grid.screen_center_world_y
-        self.change_label(x,y)
-            
-    def hover_leave(self,event):
-        self.change_label("","")
-
-    def change_label(self,center_x,center_y):
-        if center_x == "":
-            self.label_coor.config(text='Coordinates  x: ---  y: ---' )
-        else:
-            center_x,center_y = int(center_x), int(center_y)
-            self.label_coor.grid(row=self.row,column=self.column+1,sticky=SE)
-            self.label_coor.config(text=f'Coordinates  x:{center_x}  y:{center_y}' )
-
-    def mouse_wheel(self,event):
-        zoom_in = 0
-        if event.delta == -120:
-            if self.scale_step <= self.MAX_ZOOM:
-                zoom_in = -1
-                self.scale_step +=1
-        if event.delta == 120:
-            if self.scale_step >= self.MIN_ZOOM:
-                zoom_in = 1
-                self.scale_step -=1
-        self.world_grid.zoom(event.x,event.y,zoom_in)
-
-    def pan_release(self,event):
-        self.pan_x1, self.pan_y1 = None, None
-        self.canvas.config(cursor='tcross')
-
-    def pan_move(self,event):
-        try:
-            x2, y2 = event.x, event.y
-            self.world_grid.pan_move(x2-self.pan_x1, self.pan_y1-y2)
-            self.pan_x1, self.pan_y1 = event.x, event.y
-        except:
-            self.pan_x1, self.pan_y1 = event.x, event.y
-        sleep(0.05)
-        self.canvas.config(cursor='circle')
-
-    def add_background(self):
-        filepath = filedialog.askopenfilename(initialdir=self.initialdir)
-        self.world_grid.add_background(filepath)
-
-    def drawline(self,event):
-        if self.draw_x1 == None:
-            self.draw_x1, self.draw_y1 = event.x, event.y
-        else:
-            x2, y2 = event.x, event.y
-            self.world_grid.draw_s_line(self.draw_x1,self.draw_y1,x2,y2)
-            self.draw_x1, self.draw_y1 = None,None
-
-    def draw_rectangle(self,event):
-        print('a')
-        if self.draw_x1 == None:
-            self.draw_x1, self.draw_y1 = event.x, event.y
-        else:
-            x2, y2 = event.x, event.y
-            self.world_grid.draw_rectangle(self.draw_x1,self.draw_y1,x2,y2)
-            self.draw_x1, self.draw_y1 = None,None
-
-    # def drawline_end(self,event):
-    #     self.draw_x1, self.draw_y1 = None, None
-
-    def change_draw_label(self):
-        self.label_status.config(text='Status: s_line Specify first point')
-
 
 
 class ToolBoxTab():
@@ -161,7 +48,7 @@ class ToolBoxTab():
         my_notebook.add(my_frame2,text='Insert')
         my_notebook.add(my_frame3,text='Annotate')
 
-        f1_button = ttk.Button(my_frame1,text= 'hihi',command=lambda:canvas.change_draw_label())
+        f1_button = ttk.Button(my_frame1,text= 'hihi',command=lambda:canvas.draw_shape.change_draw_label())
         f1_button.grid(row=0,column=0,sticky=W)
 
 class PageTab():
@@ -200,7 +87,7 @@ if __name__ == "__main__":
     pagetab = PageTab(root,1,1,1,0,SW)
     canvas = WindowCanvas(pagetab.page_frame,width * 0.7,height-190,2,0,SW)
 
-    # canvas.world_grid.add_background('imag/B1.jpg')
+    canvas.world_grid.add_background('imag/B1.jpg')
 
     root.mainloop()
 
---
+++ program/canvas.py
@@ -0,0 +1,160 @@
+from tkinter import filedialog
+from tkinter import *
+from tkinter import ttk
+from ttkthemes import ThemedStyle
+# from pdf2image import convert_fr om_path
+from enum import Enum
+import os
+import time
+from PIL import ImageTk,Image
+from relative_grid import *
+
+class HexColor(Enum):
+    BACKGROUND = '#2d2d2d'
+    TITLE = '#222933'
+    TOOLTAB = '#3B4453'
+    CANVAS_BACKGROUD = '#222831'
+
+class WindowCanvas():
+    def __init__(self,app,width,height,row,column,sticky):
+        self.width, self.height = width, height
+        self.bkgd_color = HexColor.CANVAS_BACKGROUD.value
+        self.row, self.column = row, column
+        self.app = app
+
+        self.canvas = Canvas(app,width=self.width,height=self.height,background=self.bkgd_color,
+                        cursor='tcross',borderwidth=0,highlightthickness=0,relief=FLAT)
+        self.canvas.grid(row=row,column=column,columnspan=2,sticky=sticky)  
+
+        self.label_coor = ttk.Label(app,text='Coordinates  x: ---  y: ---')
+        self.label_coor.grid(row=row,column=column+1,sticky=SE)
+        self.label_status = ttk.Label(app,text='Status: pan')
+        self.label_status.grid(row=row,column=column,sticky=SW)
+        self.world_grid = WorldGrid(self.width,self.height,self.canvas)
+
+        self.key_binding()
+
+    # set focus on canvas for drawing
+        self.canvas.focus_set()
+
+    # set key binding for canvas
+    def key_binding(self):
+        self.hover_coor = HoverCoor(self)
+        self.canvas.bind("<Motion>", self.hover_coor.hover_motion)
+        self.canvas.bind("<Leave>", self.hover_coor.hover_leave)
+
+        self.zoom_and_pan = PanAndZoom(self)
+        self.canvas.bind("<MouseWheel>", self.zoom_and_pan.mouse_wheel)
+        self.canvas.bind("<B1-Motion>", self.zoom_and_pan.pan_move)
+        self.canvas.bind("<B1-ButtonRelease>", self.zoom_and_pan.pan_release)
+        self.canvas.bind("<B2-Motion>", self.zoom_and_pan.pan_move)
+        self.canvas.bind("<B2-ButtonRelease>", self.zoom_and_pan.pan_release)
+
+        self.draw_shape = DrawShape(self)
+        self.canvas.bind("<Button-3>", self.draw_shape.drawline)
+        self.canvas.bind("a",self.draw_shape.draw_rectangle)
+
+        self.canvas.bind("<Button-1>",self.set_focus)
+        self.canvas.bind("<Button-2>",self.set_focus)
+        
+    def set_focus(self,event):
+        event.widget.focus_set()
+
+    @property
+    def scale_step(self):
+        step = self.zoom_and_pan.scale_step
+        return step
+
+class HoverCoor():
+    def __init__(self,WindowCanvas : WindowCanvas):
+        self.win_can = WindowCanvas
+        self.world_grid = WindowCanvas.world_grid
+        self.label_coor = WindowCanvas.label_coor
+        self.width = WindowCanvas.width
+        self.height = WindowCanvas.height
+        self.row = WindowCanvas.row
+        self.column = WindowCanvas.column
+
+    def hover_motion(self,event):
+        scale_step = self.win_can.scale_step
+        x,y = self.world_grid.screen_to_world(event.x,event.y,scale_step,self.width,self.height)
+        x += self.world_grid.screen_center_world_x
+        y += self.world_grid.screen_center_world_y
+        self.change_label(x,y)
+            
+    def hover_leave(self,event):
+        self.change_label("","")
+
+    def change_label(self,center_x,center_y):
+        if center_x == "":
+            self.label_coor.config(text='Coordinates  x: ---  y: ---' )
+        else:
+            center_x,center_y = int(center_x), int(center_y)
+            self.label_coor.grid(row=self.row,column=self.column+1,sticky=SE)
+            self.label_coor.config(text=f'Coordinates  x:{center_x}  y:{center_y}' )
+
+class PanAndZoom():
+    def __init__(self,WindowCanvas : WindowCanvas):
+        self.world_grid = WindowCanvas.world_grid
+        self.canvas = WindowCanvas.canvas
+        self.MAX_ZOOM = 30
+        self.MIN_ZOOM = -25
+        self.scale_step = 0
+
+    def mouse_wheel(self,event):
+        zoom_in = 0
+        if event.delta == -120:
+            if self.scale_step <= self.MAX_ZOOM:
+                zoom_in = -1
+                self.scale_step +=1
+        if event.delta == 120:
+            if self.scale_step >= self.MIN_ZOOM:
+                zoom_in = 1
+                self.scale_step -=1
+        self.world_grid.zoom(event.x,event.y,zoom_in)
+
+    def pan_release(self,event):
+        self.pan_x1, self.pan_y1 = None, None
+        self.canvas.config(cursor='tcross')
+
+    def pan_move(self,event):
+        try:
+            x2, y2 = event.x, event.y
+            self.world_grid.pan_move(x2-self.pan_x1, self.pan_y1-y2)
+            self.pan_x1, self.pan_y1 = event.x, event.y
+        except:
+            self.pan_x1, self.pan_y1 = event.x, event.y
+        sleep(0.05)
+        self.canvas.config(cursor='circle')
+
+class DrawShape():
+    def __init__(self,WindowCanvas : WindowCanvas):
+        self.draw_x1, self.draw_y1 = None,None
+        self.world_grid = WindowCanvas.world_grid
+        self.label_status = WindowCanvas.label_status
+        
+    def add_background(self):
+        filepath = filedialog.askopenfilename(initialdir=self.initialdir)
+        self.world_grid.add_background(filepath)
+
+    def drawline(self,event):
+        if self.draw_x1 == None:
+            self.draw_x1, self.draw_y1 = event.x, event.y
+        else:
+            x2, y2 = event.x, event.y
+            self.world_grid.draw_s_line(self.draw_x1,self.draw_y1,x2,y2)
+            self.draw_x1, self.draw_y1 = None,None
+
+    def draw_rectangle(self,event):
+        if self.draw_x1 == None:
+            self.draw_x1, self.draw_y1 = event.x, event.y
+        else:
+            x2, y2 = event.x, event.y
+            self.world_grid.draw_rectangle(self.draw_x1,self.draw_y1,x2,y2)
+            self.draw_x1, self.draw_y1 = None,None
+
+    def change_draw_label(self):
+        self.label_status.config(text='Status: s_line Specify first point')
+
+if __name__ == "__main__":
+    pass 
